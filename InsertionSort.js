5887cf89f8a57d17d8b7f73cd2e50b05a39e6e0d
Sriram Murali <1sri93ram@gmail.com>
2021-04-01 21:56:22 +0200

    Formatted

--- InsertionSort.js
+++ InsertionSort.js
@@ -4,17 +4,17 @@
                     O(n^2) - Worst Case 
 */
 
-var arr = [5,4,3,2,1]
+var arr = [5, 4, 3, 2, 1];
 var n = arr.length;
 
-for(var i=1;i< n;i ++){
-    var key = arr[i]
-    j = i-1;
+for (var i = 1; i < n; i++) {
+  var key = arr[i];
+  j = i - 1;
 
-    while(j >= 0 && arr[j] > key){
-        arr[j+1] = arr[j]
-        j = j-1
-    }
-    arr[j + 1] = key; 
-    console.log("Hello");
+  while (j >= 0 && arr[j] > key) {
+    arr[j + 1] = arr[j];
+    j = j - 1;
+  }
+  arr[j + 1] = key;
+  console.log("Hello");
 }
